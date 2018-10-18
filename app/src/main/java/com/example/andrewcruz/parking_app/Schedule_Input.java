a117319cf6461d9a1ed04c7bd5c18f57d4034036
Andrew Cruz <andrewcruz1000@gmail.com>
2018-10-17 21:13:16 -0700

    Made schedule mail button switch to main activiy

--- app/src/main/java/com/example/andrewcruz/parking_app/Schedule_Input.java
+++ app/src/main/java/com/example/andrewcruz/parking_app/Schedule_Input.java
@@ -1,5 +1,6 @@
 package com.example.andrewcruz.parking_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -20,8 +21,7 @@ public class Schedule_Input extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(Schedule_Input.this, MainActivity.class));
             }
         });
     }
