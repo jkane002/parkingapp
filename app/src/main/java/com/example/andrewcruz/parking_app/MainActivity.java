acf4715aff4ee52730f7b25cc0bee7bd6917307c
Andrew Cruz <andrewcruz1000@gmail.com>
2018-10-17 21:07:11 -0700

    Made mail button switch to schedule activiy

--- app/src/main/java/com/example/andrewcruz/parking_app/MainActivity.java
+++ app/src/main/java/com/example/andrewcruz/parking_app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.andrewcruz.parking_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -18,16 +19,19 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        THIS IS THE MAIL BUTTON ON BOTTOM RIGHT
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(MainActivity.this, Schedule_Input.class));
             }
         });
     }
 
+
+    //THIS IS THE THREE BUTTONS ON TOP RIGHT
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
