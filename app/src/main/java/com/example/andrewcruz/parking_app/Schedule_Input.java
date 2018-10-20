ff33a8e6ce312dd38b7aa2938baa20f481b0634f
Andrew Cruz <andrewcruz1000@gmail.com>
2018-10-20 13:14:19 -0700

    Added New bulding Activity

--- app/src/main/java/com/example/andrewcruz/parking_app/Schedule_Input.java
+++ app/src/main/java/com/example/andrewcruz/parking_app/Schedule_Input.java
@@ -6,9 +6,16 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.w3c.dom.Text;
+
 
 public class Schedule_Input extends AppCompatActivity {
+    private static final String TAG = "Schedule Input Activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +24,20 @@ public class Schedule_Input extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(Schedule_Input.this, MainActivity.class));
+//            }
+//        });
+
+
+        Button bldging_bttn = (Button) findViewById(R.id.add_building);
+        bldging_bttn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(Schedule_Input.this, MainActivity.class));
+                Log.d(TAG,"in onclick()");
             }
         });
     }
