b38943c0a0cb249dfc70fcbdf6a54f2609bf8f1c
Andrew Cruz <andrewcruz1000@gmail.com>
2018-10-24 13:15:25 -0700

    Added List and onclick()

--- app/src/main/java/com/example/andrewcruz/parking_app/Building_Selection.java
+++ app/src/main/java/com/example/andrewcruz/parking_app/Building_Selection.java
@@ -1,5 +1,6 @@
 package com.example.andrewcruz.parking_app;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,9 +10,12 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 
 public class Building_Selection extends AppCompatActivity {
--- app/src/main/java/com/example/andrewcruz/parking_app/ListViewBuildings.java
+++ app/src/main/java/com/example/andrewcruz/parking_app/ListViewBuildings.java
@@ -1,14 +1,29 @@
 package com.example.andrewcruz.parking_app;
 
 import android.app.ListActivity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
+
+
 
 public class ListViewBuildings extends AppCompatActivity {
+    private static final String tag = "ListViewBuilding Act";
     ListView simpleList;
-    String[] buildings = {"Chung", "MSE", "BLANK"};
+    String[] buildings = {"Bourns Hall", "Boyce Hall", "CHASS INT NORTH", "CHASS INT SOUTH", "CHUNG HALL",
+            "HUMANITIES/SOCIAL SCIENCE", "LIFE SCIRENCES", "MATERIAL SCIENCE & ENGINEERING", "OLMSTED HALL",
+            "PHYSICS", "PIERCE HALL", "SPROUL HALL", "SKYE", "SURGE FACILITY", "SPIETH HALL", "UV THEATER",
+            "UNLH", "WATKINS"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +33,14 @@ public class ListViewBuildings extends AppCompatActivity {
 
         simpleList = (ListView)findViewById(R.id.simpleListView);
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_building__selection, R.id.textView, buildings);
-//        ArrayAdapter<String> adapter = new ArrayAdapter<String>( this, android.R.layout.simple_list_item_1, buildings);
         simpleList.setAdapter(arrayAdapter);
+
+        simpleList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String txt = "clicked on " + buildings[position];
+                Log.d(tag, txt);
+            }
+        });
     }
 }
