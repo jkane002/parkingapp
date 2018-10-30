78551b2506f322b86295ec9133bf7aa610ac8cc8
Andrew Cruz <andrewcruz1000@gmail.com>
2018-10-29 21:58:38 -0700

    Merged All Activities

--- app/src/main/java/com/example/andrewcruz/parking_app/MainActivity.java
+++ app/src/main/java/com/example/andrewcruz/parking_app/MainActivity.java
@@ -9,50 +9,47 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+    public class MainActivity extends AppCompatActivity {
+        ListView listView;
+        //  TextView textview;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            listView = (ListView)findViewById(R.id.listView);
 
-public class MainActivity extends AppCompatActivity {
+            final String[] values = new String[]{"Parking List Web View", "Schedule Input", "TAPS Information"};
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                    R.layout.activity_list__main,android.R.id.text1, values);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+            listView.setAdapter(adapter);
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view,
+                                        int position, long id) {
+                    if (position == 0) {
+                        Intent myintent  = new Intent(view.getContext(), Parking_View.class );
+                        startActivityForResult(myintent,0);
 
-        TextView bldging_bttn = (TextView) findViewById(R.id.sched_txt);
+                    }
+                    if (position == 1) {
+                        Intent myintent  = new Intent(view.getContext(), Schedule_Input.class );
+                        startActivityForResult(myintent,1);
 
-        bldging_bttn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, Schedule_Input.class));
-            }
-        });
-
-    }
-
-    //THIS IS THE THREE BUTTONS ON TOP RIGHT
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                    }
+                    if (position == 2) {
+                        Intent myintent  = new Intent(view.getContext(), Taps_View.class );
+                        startActivityForResult(myintent,2);
+                    }
+                }
+            });
         }
-
-        return super.onOptionsItemSelected(item);
     }
-}
+
+
+
