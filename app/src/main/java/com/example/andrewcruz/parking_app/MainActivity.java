48160bdd54cad05cdaa4b99647690b350653ed0f
Andrew Cruz <andrewcruz1000@gmail.com>
2018-11-13 22:00:27 -0800

    commit

--- app/src/main/java/com/example/andrewcruz/parking_app/MainActivity.java
+++ app/src/main/java/com/example/andrewcruz/parking_app/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.andrewcruz.parking_app;
 
 import android.app.Dialog;
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -26,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
+
 
             if(isServicesOK()) {
                 init();
@@ -80,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
             }
             return false;
         }
+
     }
 
 
