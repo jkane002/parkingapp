4a933149cb8258cdba96a6998cc83befff63df9f
Andrew Cruz <andrewcruz1000@gmail.com>
2018-11-21 11:45:27 -0800

    firebase depends all added

--- app/src/main/java/com/example/andrewcruz/parking_app/FireBase_ParkingApp.java
+++ app/src/main/java/com/example/andrewcruz/parking_app/FireBase_ParkingApp.java
@@ -1,13 +1,13 @@
 package com.example.andrewcruz.parking_app;
 
 import android.app.Application;
-//import com.firebase.client.Firebase;
+import com.firebase.client.Firebase;
 
 public class FireBase_ParkingApp extends Application {
-//
-//    @Override
-//    public void onCreate() {
-//        super.onCreate();
-//        Firebase.set
-//    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Firebase.set
+    }
 }
